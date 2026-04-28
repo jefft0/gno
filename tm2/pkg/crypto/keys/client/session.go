1017444c9f9cb2526cb23506df84bcfbf0c7d2dc
Jeff Thompson <jeff@thefirst.org>
2026-04-28 14:58:57 +0200

    feat(gnokey): Add session support
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

---
+++ tm2/pkg/crypto/keys/client/session.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"context"
+	"flag"
+
+	"github.com/gnolang/gno/tm2/pkg/commands"
+)
+
+type SessionCfg struct {
+	RootCfg *MakeTxCfg
+}
+
+func NewSessionCmd(rootCfg *MakeTxCfg, io commands.IO) *commands.Command {
+	cfg := &SessionCfg{
+		RootCfg: rootCfg,
+	}
+
+	cmd := commands.NewCommand(
+		commands.Metadata{
+			Name:       "session",
+			ShortUsage: "session <subcommand> [flags]",
+			ShortHelp:  "create or revoke subaccount sessions",
+		},
+		cfg,
+		func(_ context.Context, args []string) error {
+			return execSession(cfg, args, io)
+		},
+	)
+
+	cmd.AddSubCommands(
+		NewSessionCreateCmd(cfg, io),
+		NewSessionRevokeCmd(cfg, io),
+		NewSessionRevokeAllCmd(cfg, io),
+	)
+
+	return cmd
+}
+
+func (c *SessionCfg) RegisterFlags(fs *flag.FlagSet) {
+}
+
+func execSession(cfg *SessionCfg, args []string, io commands.IO) error {
+	return nil
+}
